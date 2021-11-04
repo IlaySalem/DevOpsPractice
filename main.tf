c10aadf96d4b0719393d07e3548416d089c7b728
IlaySalem <ilaysalem@yandex.ru>
2021-11-05 01:43:48 +0300

    выпелен докер, добавлено выполнение скрипта

--- main.tf
+++ main.tf
@@ -3,11 +3,7 @@ terraform {
     aws = {
       source = "hashicorp/aws"
       version = "3.63.0"
-    }
-	docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.15.0"
-    }
+    }	
   }
 }
 
@@ -17,7 +13,7 @@ resource "tls_private_key" "pk" {
 }
 
 resource "aws_key_pair" "kp" {
-  key_name   = "myKey"
+  key_name   = "myKey_2"
   public_key = tls_private_key.pk.public_key_openssh
   #public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCIaLGKyipshahy3rslmsptOqJFYPJQhbuCC5oXBumfgatBUFvuTbFZ1uAs1Vk15bV2+HJHEik1uon5IqaDdaCc/YXh7lw4ssM6DgJTBxSZG1guNscsBjFPpe8MgWqFYlZBdjjmDixV07kY9G5dBg1pzdNwiAj3jTYfJ9ZZnPCreD2V7eYie8snTGcWXKF2F5hje8zWUVE6xHbF2opFCUhrzYiB5EMADpcML2aFZPyhRRk5XxKOw1CEOFA"
   provisioner "local-exec" {	
@@ -46,32 +42,16 @@ resource "aws_instance" "first_vm" {
   key_name      = aws_key_pair.kp.key_name
   instance_type = "t2.micro"
 
+	user_data = <<-EOF
+	#!/!bin/bash
+	echo "*** INSTALLING NGINX"
+	sudo apt update -y
+	sudo apt install nginx -y
+	sudo systemctl start nginx
+	echo "*** COMPLITED INSTALLATION NGINX"
+	EOF
+
   tags = {
     Name = "HelloWorld"
-  }
-}
-
-# Pulls the image
-resource "docker_image" "ubuntu" {
-  name = "ubuntu:latest"
-}
-
-# Create a container
-resource "docker_container" "foo" {
-  image = docker_image.ubuntu.latest
-  name  = "foo"
-}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
   }
 }
--- provider.tf
+++ provider.tf
@@ -1,7 +1,3 @@
 provider "aws" {
   region = "eu-west-3"
 }
-
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
